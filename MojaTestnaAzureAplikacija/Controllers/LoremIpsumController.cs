4238d0ba0d8f3554427f768757ea82f6e1603bd3
LeaRezic <lea.rezic@gmail.com>
2018-05-23 20:28:16 +0200

    Initial commit

---
+++ MojaTestnaAzureAplikacija/Controllers/LoremIpsumController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MojaTestnaAzureAplikacija.Controllers
+{
+    public class LoremIpsumController : Controller
+    {
+        // GET: LoremIpsum
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
